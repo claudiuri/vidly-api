d87ab93a32e22ce597401f80bed14b0bec72bcde
Cláudio Yuri <claudio.y07@gmail.com>
2019-07-17 22:18:04 -0300

    add endpoint para movies and rentals

--- models/genre.js
+++ models/genre.js
@@ -1,14 +1,17 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50
-    }
-}));
+
+const genreSchema =  new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
   const schema = {
@@ -19,4 +22,5 @@ function validateGenre(genre) {
 }
 
 exports.Genre = Genre;
+exports.genreSchema = genreSchema;
 exports.validate = validateGenre;
