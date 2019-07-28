3e94e2dc7519677945987ccf1d15d6cff734412a
Cláudio Yuri <claudio.y07@gmail.com>
2019-07-27 23:55:27 -0300

    first integration test

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const winston = require('winston');
 const express = require('express');
 const app = express();
 
-require('./startup/loggin')
+require('./startup/loggin')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+module.exports = server;
