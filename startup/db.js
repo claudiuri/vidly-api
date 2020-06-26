e6514b42c6006929055897f016089b04d3521292
claudiuri <claudio.y07@gmail.com>
2020-06-25 21:02:29 -0300

    added mongodb in memory for test

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,19 @@
 const config  = require('config');
 const winston = require('winston');
 const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-module.exports = function () {
-    const db = process.env.DB || config.get('db');
+module.exports = async function () {
+    let db = process.env.DB || config.get('db');
+
+    if (process.env.NODE_ENV === 'test') {
+        const mongod = new MongoMemoryServer();
+
+        db = await mongod.getConnectionString()
+
+        console.log(db);
+    }
+
     mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
         .then(() => winston.info(`Connected to ${db}...`))
 }
